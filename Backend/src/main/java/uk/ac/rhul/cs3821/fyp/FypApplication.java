08845ee75a392974df0881ca96423489a39f1cb4
Maryam Kamar <ZMAC266@live.rhul.ac.uk>
2025-10-12 22:27:11 +0100

    #19 initialised backend with cloud database connection

---
+++ Backend/src/main/java/uk/ac/rhul/cs3821/fyp/FypApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs3821.fyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FypApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/uk/ac/rhul/cs3821/fyp/FypApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2800.fyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
