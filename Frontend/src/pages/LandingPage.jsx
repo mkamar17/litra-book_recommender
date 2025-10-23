c6144db234cc503c0067085dacc56ce3831166c1
Maryam Kamar <ZMAC266@live.rhul.ac.uk>
2025-10-23 20:17:18 +0100

    #22 nav bar implemented - small issue with alignment of notifs and profile - also search bar missing

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../api/api.js";
 import BookRow from "../components/BookRow";
+import NavBar from "../components/NavBar.jsx"
 import '../App.css'
 
 export default function LandingPage() {
@@ -13,8 +14,9 @@ export default function LandingPage() {
   }, []);
 
   return (
-    <div className="bg-grey min-h-screen text-white font-poppins">
-    <div className="text-left space-y-10 px-10">
+    <div className="bg-[rgb(24,24,24)] min-h-screen text-white font-poppins">
+    <NavBar />
+    <div className="text-left space-y-10 px-10 mt-6">
         <BookRow title="For You" books={books} />
     </div>
     </div>
