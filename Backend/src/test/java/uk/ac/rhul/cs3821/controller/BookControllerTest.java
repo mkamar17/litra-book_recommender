41ae9c5aadcb07f83b5b442af769a52d0fb40030
Maryam Kamar <ZMAC266@live.rhul.ac.uk>
2025-10-30 13:42:44 +0000

    #24 fixed BookControllerTest

--- Backend/src/test/java/uk/ac/rhul/cs3821/controller/BookControllerTest.java
+++ Backend/src/test/java/uk/ac/rhul/cs3821/controller/BookControllerTest.java
@@ -3,6 +3,7 @@ package uk.ac.rhul.cs3821.controller;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookController.class)
+@AutoConfigureMockMvc(addFilters = false) //to stop unauthorisation errors due to default security configs
 class BookControllerTest {
 
     @Autowired
