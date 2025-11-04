2c860be2a7e7e3634998ecb02c8df80d42256c07
Maryam Kamar <ZMAC266@live.rhul.ac.uk>
2025-11-04 14:20:26 +0000

    #25 fixed recommendationcontroller checkstyle errors

--- Backend/src/main/java/uk/ac/rhul/cs3821/controller/RecommendationController.java
+++ Backend/src/main/java/uk/ac/rhul/cs3821/controller/RecommendationController.java
@@ -2,20 +2,25 @@ package uk.ac.rhul.cs3821.controller;
 
 import java.util.List;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller currently acts as a protected sample endpoint.
- *
- * @return static list of books
- *
  */
+
 @RestController
 @RequestMapping("/recommendations")
 public class RecommendationController {
-    @GetMapping
-    public List<String> get(Authentication auth) {
-        // demo: use auth.getName() to personalise
-        return List.of("Fourth Wing", "The Women", "Where the Crawdads Sing");
-    }
+  /**
+   * Get method returns static list of books.
+   *
+   * @param auth to authenticate user.
+   * @return static book list.
+   */
+  @GetMapping
+  public List<String> get(Authentication auth) {
+    return List.of("Fourth Wing", "The Women", "Where the Crawdads Sing");
+  }
 }
