55f14fd5ea30d932490bc6583fb63dd52075d471
Maryam Kamar <ZMAC266@live.rhul.ac.uk>
2026-04-06 14:25:40 +0100

    #59 Removed console log with security vulnerability

--- product/Frontend/src/api/api.js
+++ product/Frontend/src/api/api.js
@@ -7,8 +7,6 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    console.log("TOKEN:", localStorage.getItem("token"));
-
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
